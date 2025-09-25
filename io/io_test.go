11b5dc0587ea0ced88305ed25d36d684ba4405ae
root <root@hades.localdomain>
2025-09-25 20:50:35 +0800

    websocket client docs

--- io/io_test.go
+++ io/io_test.go
@@ -1,10 +1,11 @@
-package ooo_test
+package io_test
 
 import (
 	"os"
 	"testing"
 
 	"github.com/benitogf/ooo"
+	"github.com/benitogf/ooo/io"
 	"github.com/stretchr/testify/require"
 )
 
@@ -23,45 +24,45 @@ func TestIObasic(t *testing.T) {
 	server.Silence = true
 	server.Start("localhost:0")
 	defer server.Close(os.Interrupt)
-	err := ooo.Set(server, THING1_PATH, Thing{
+	err := io.Set(server, THING1_PATH, Thing{
 		This: "this",
 		That: "that",
 	})
 	require.NoError(t, err)
-	err = ooo.Set(server, THING2_PATH, Thing{
+	err = io.Set(server, THING2_PATH, Thing{
 		This: "here",
 		That: "there",
 	})
 	require.NoError(t, err)
 
-	thing1, err := ooo.Get[Thing](server, THING1_PATH)
+	thing1, err := io.Get[Thing](server, THING1_PATH)
 	require.NoError(t, err)
 
 	require.Equal(t, "this", thing1.Data.This)
 	require.Equal(t, "that", thing1.Data.That)
 
-	thing2, err := ooo.Get[Thing](server, THING2_PATH)
+	thing2, err := io.Get[Thing](server, THING2_PATH)
 	require.NoError(t, err)
 
 	require.Equal(t, "here", thing2.Data.This)
 	require.Equal(t, "there", thing2.Data.That)
 
-	err = ooo.Push(server, THINGS_PATH, thing1)
+	err = io.Push(server, THINGS_PATH, thing1)
 	require.NoError(t, err)
-	err = ooo.Push(server, THINGS_PATH, thing2)
+	err = io.Push(server, THINGS_PATH, thing2)
 	require.NoError(t, err)
 
-	things, err := ooo.GetList[Thing](server, THINGS_PATH)
+	things, err := io.GetList[Thing](server, THINGS_PATH)
 	require.NoError(t, err)
 	require.Equal(t, 2, len(things))
 
-	err = ooo.Set(server, string(THINGS_BASE_PATH)+"/what", Thing{
+	err = io.Set(server, string(THINGS_BASE_PATH)+"/what", Thing{
 		This: "what",
 		That: "how",
 	})
 	require.NoError(t, err)
 
-	things, err = ooo.GetList[Thing](server, THINGS_PATH)
+	things, err = io.GetList[Thing](server, THINGS_PATH)
 	require.NoError(t, err)
 	require.Equal(t, 3, len(things))
 	require.Equal(t, "what", things[2].Data.This)
@@ -73,45 +74,45 @@ func TestRemoteIO(t *testing.T) {
 	server.Start("localhost:0")
 	defer server.Close(os.Interrupt)
 
-	err := ooo.RemoteSet(server.Client, false, server.Address, THING1_PATH, Thing{
+	err := io.RemoteSet(server.Client, false, server.Address, THING1_PATH, Thing{
 		This: "this",
 		That: "that",
 	})
 	require.NoError(t, err)
-	err = ooo.RemoteSet(server.Client, false, server.Address, THING2_PATH, Thing{
+	err = io.RemoteSet(server.Client, false, server.Address, THING2_PATH, Thing{
 		This: "here",
 		That: "there",
 	})
 	require.NoError(t, err)
 
-	thing1, err := ooo.RemoteGet[Thing](server.Client, false, server.Address, THING1_PATH)
+	thing1, err := io.RemoteGet[Thing](server.Client, false, server.Address, THING1_PATH)
 	require.NoError(t, err)
 
 	require.Equal(t, "this", thing1.Data.This)
 	require.Equal(t, "that", thing1.Data.That)
 
-	thing2, err := ooo.RemoteGet[Thing](server.Client, false, server.Address, THING2_PATH)
+	thing2, err := io.RemoteGet[Thing](server.Client, false, server.Address, THING2_PATH)
 	require.NoError(t, err)
 
 	require.Equal(t, "here", thing2.Data.This)
 	require.Equal(t, "there", thing2.Data.That)
 
-	err = ooo.RemotePush(server.Client, false, server.Address, THINGS_PATH, thing1)
+	err = io.RemotePush(server.Client, false, server.Address, THINGS_PATH, thing1)
 	require.NoError(t, err)
-	err = ooo.RemotePush(server.Client, false, server.Address, THINGS_PATH, thing2)
+	err = io.RemotePush(server.Client, false, server.Address, THINGS_PATH, thing2)
 	require.NoError(t, err)
 
-	things, err := ooo.RemoteGetList[Thing](server.Client, false, server.Address, THINGS_PATH)
+	things, err := io.RemoteGetList[Thing](server.Client, false, server.Address, THINGS_PATH)
 	require.NoError(t, err)
 	require.Equal(t, 2, len(things))
 
-	err = ooo.RemoteSet(server.Client, false, server.Address, string(THINGS_BASE_PATH)+"/what", Thing{
+	err = io.RemoteSet(server.Client, false, server.Address, string(THINGS_BASE_PATH)+"/what", Thing{
 		This: "what",
 		That: "how",
 	})
 	require.NoError(t, err)
 
-	things, err = ooo.RemoteGetList[Thing](server.Client, false, server.Address, THINGS_PATH)
+	things, err = io.RemoteGetList[Thing](server.Client, false, server.Address, THINGS_PATH)
 	require.NoError(t, err)
 	require.Equal(t, 3, len(things))
 	require.Equal(t, "what", things[2].Data.This)
--- io/remote.go
+++ io/remote.go
@@ -1,4 +1,4 @@
-package ooo
+package io
 
 import (
 	"bytes"
@@ -12,111 +12,6 @@ import (
 	"github.com/benitogf/ooo/meta"
 	"github.com/goccy/go-json"
 )
-
-func GetList[T any](server *Server, path string) ([]client.Meta[T], error) {
-	lastPath := key.LastIndex(path)
-	isList := lastPath == "*"
-
-	var result []client.Meta[T]
-	if !isList {
-		return result, errors.New("GetList[" + path + "]: path is not a list")
-	}
-	raw, err := server.Storage.Get(path)
-	if err != nil {
-		log.Println("GetList["+path+"]: failed to get from storage", err)
-		return result, err
-	}
-	objs, err := meta.DecodeList(raw)
-	if err != nil {
-		log.Println("GetList["+path+"]: failed to decode data", err)
-		return result, err
-	}
-	for _, obj := range objs {
-		var item T
-		err = json.Unmarshal([]byte(obj.Data), &item)
-		if err != nil {
-			log.Println("GetList["+path+"]: failed to unmarshal data", err)
-			continue
-		}
-		result = append(result, client.Meta[T]{
-			Created: obj.Created,
-			Updated: obj.Updated,
-			Index:   obj.Index,
-			Data:    item,
-		})
-	}
-	return result, nil
-}
-
-func Get[T any](server *Server, path string) (client.Meta[T], error) {
-	lastPath := key.LastIndex(path)
-	isList := lastPath == "*"
-
-	var result client.Meta[T]
-	if isList {
-		return result, errors.New("Get[" + path + "]: path is a list")
-	}
-
-	raw, err := server.Storage.Get(path)
-	if err != nil {
-		log.Println("Get["+path+"]: failed to get from storage", err)
-		return result, err
-	}
-	obj, err := meta.Decode(raw)
-	if err != nil {
-		log.Println("Get["+path+"]: failed to decode data", err)
-		return result, err
-	}
-	var item T
-	err = json.Unmarshal([]byte(obj.Data), &item)
-	if err != nil {
-		log.Println("Get["+path+"]: failed to unmarshal data", err)
-		return result, err
-	}
-	result = client.Meta[T]{
-		Created: obj.Created,
-		Updated: obj.Updated,
-		Index:   obj.Index,
-		Data:    item,
-	}
-	return result, nil
-}
-
-func Set[T any](server *Server, path string, item T) error {
-	lastPath := key.LastIndex(path)
-	isList := lastPath == "*"
-
-	if isList {
-		return errors.New("Set[" + path + "]: path is a list")
-	}
-
-	data, err := json.Marshal(item)
-	if err != nil {
-		log.Println("Set["+path+"]: failed to marshal data", err)
-		return err
-	}
-	_, err = server.Storage.Set(path, data)
-	return err
-}
-
-func Push[T any](server *Server, path string, item T) error {
-	lastPath := key.LastIndex(path)
-	isList := lastPath == "*"
-
-	if !isList {
-		return errors.New("Push[" + path + "]: path is not a list")
-	}
-
-	_path := key.Build(path)
-
-	data, err := json.Marshal(item)
-	if err != nil {
-		log.Println("Push["+path+"]: failed to marshal data", err)
-		return err
-	}
-	_, err = server.Storage.Set(_path, data)
-	return err
-}
 
 func RemoteSet[T any](_client *http.Client, ssl bool, host string, path string, item T) error {
 	lastPath := key.LastIndex(path)
